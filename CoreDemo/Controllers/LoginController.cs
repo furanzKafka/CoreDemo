c67dc4086ad5e186cf9fb28fa45e831417a81559
furanzKafka <furkankizilkoca@gmail.com>
2023-01-02 01:33:58 +0300

    add auth

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+		[AllowAnonymous]
+		[HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            Context c = new Context();
+            var dataValue=c.Writers.FirstOrDefault(x=>x.Mail==writer.Mail&& x.Password==writer.Password);
+            if (dataValue!=null)
+            {
+                HttpContext.Session.SetString("username", writer.Mail);
+                return RedirectToAction("Index","Blog");
+            }
+            else
+            {
+				return View();
+			}
+		}
     }
 }
