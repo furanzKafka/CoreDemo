ec7937156ea0cf7dbe1108e1f97590a11aa33d91
furanzKafka <furkankizilkoca@gmail.com>
2023-01-10 19:15:11 +0300

    add weather forecast using api but not test, try it bro

--- CoreDemo/Areas/Admin/ViewComponents/Statistics/Statistic.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistics/Statistic.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistics
 {
@@ -14,6 +15,13 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistics
             ViewBag.blogCount = bm.GetAll().Count;
             ViewBag.contactCount = cm.GetAll().Count;
             ViewBag.commentCount = commentManager.GetAll().Count;
+
+            string myWeatherAPI = "7b054f311eb8bfb41a0a80d056c01a79";
+            //string myConnection = "https://api.openweathermap.org/data/2.5/weather?q=London&appid="+myWeatherAPI;
+            string myConnection = "https://api.openweathermap.org/data/2.5/weather?q=London&mode=xml";
+            XDocument document=XDocument.Load(myConnection);
+            ViewBag.temperature = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
