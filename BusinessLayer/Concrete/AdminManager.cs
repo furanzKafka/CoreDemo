0e9656e91097ff36d087d31366d3e4072d815165
furanzKafka <furkankizilkoca@gmail.com>
2023-01-10 18:42:02 +0300

    before weather forecast

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _dal;
+
+        public AdminManager(IAdminDal dal)
+        {
+            _dal = dal;
+        }
+
+        public void Add(Admin about)
+        {
+            _dal.Add(about);
+        }
+        public void Delete(Admin about)
+        {
+            _dal.Delete(about);
+        }
+        public void Update(Admin about)
+        {
+            _dal.Update(about);
+        }
+        public Admin Get(int id)
+        {
+            return _dal.Get(id);
+        }
+        public List<Admin> GetAll()
+        {
+            return _dal.GetAll();
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistics/Statistic.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistics
+{
+    public class Statistic : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        ContactManager cm=new ContactManager(new EfContactRepository());
+        CommentManager commentManager=new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.blogCount = bm.GetAll().Count;
+            ViewBag.contactCount = cm.GetAll().Count;
+            ViewBag.commentCount = commentManager.GetAll().Count;
+            return View();
+        }
+    }
+}
